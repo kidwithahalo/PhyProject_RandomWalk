a222e607457bf5ac1649d607ca90af5e97b6c01e
dudewithhalo <pankajks@iitk.ac.in>
2021-08-29 10:06:03 +0530

    Create 1d_RandomWalk.py

---
+++ 1d_RandomWalk.py
@@ -0,0 +1,24 @@
+import random
+
+def random_walk(n):
+
+    x = 0
+    for i in range(n):
+        step = random.choice([-1,1])
+
+        if step == -1:
+            x = x-1
+        else:
+            x = x+1
+    return (x)
+
+
+number_of_walk = int(input("Number of walk : "))
+walk_length = int(input("How long will be the walk : "))
+
+for i in range(number_of_walk):
+    walk = random_walk(walk_length)
+    print(walk, "Distance from origin = ", abs(walk))
+
+print(f"\nFinal distance from start: {abs(walk)} ")
+    
